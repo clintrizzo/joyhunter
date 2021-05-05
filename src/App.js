aed93b77abafef5b25cd7d45fc3924a8c2572201
Noreldin Saad <s.noreldin@hotmail.com>
2021-05-04 20:13:59 -0700

    structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// This goes with the Wrapper
+import Airsoft from "./pages/Airsoft";
+import Contact from "./pages/Contact";
+import Create from "./pages/Create";
+import Homepage from "./pages/Homepage";
+import Paintball from "./pages/Paintball";
+import Profile from "./pages/Profile";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/Homepage" component={Homepage} />
+          <Route exact path="/Create" component={Create} />
+          <Route exact path="/Profile" component={Profile} />
+          <Route exact path="/Paintball" component={Paintball} />
+          <Route exact path="/Airsoft" component={Airsoft} />
+          <Route exact path="/Contact" component={Contact} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
