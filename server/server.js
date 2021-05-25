f2434953bd2dab56ec79c60feff46af6100e6ad5
Noreldin Saad <s.noreldin@hotmail.com>
2021-05-24 21:49:49 -0700

    wef

--- server/server.js
+++ server/server.js
@@ -11,6 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  app.use(express.static('client/build'));
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
