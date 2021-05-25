d6cbbbaa2c2d2279803e02fa6cc452c30101952f
Noreldin Saad <s.noreldin@hotmail.com>
2021-05-25 01:11:00 -0700

    3qr

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
