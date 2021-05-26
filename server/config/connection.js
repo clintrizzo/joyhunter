fd8507f1548d20ea2cf6e59c098e808aecbfd0c3
Clint <maxeva02@gmail.com>
2021-05-26 15:10:10 -0700

    last

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,21 +1,27 @@
-var mysql = require('mysql')
-var connection;
+var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    port:3306,
-    host:'tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
-    user:'z03ymjairscgxjet',
-    password:'y6aws1hygf7kafo3',
-    database:'p298p57zjhjzof5d',
-    }
-  );
+  var connection = mysql.createConnection({
+    host: "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "z03ymjairscgxjet",
+    password: "y6aws1hygf7kafo3",
+    database: "p298p57zjhjzof5d"
+  });
 }
 
-connection.connect();
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
+// Export connection for our ORM to use.
 module.exports = connection;
 
 
