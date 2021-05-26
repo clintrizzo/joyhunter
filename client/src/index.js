45bdadf5896de1456e40fa42b68331a3ac3d6b0f
Clint <maxeva02@gmail.com>
2021-05-25 22:14:22 -0700

    last

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const domain = AUTH0_DOMAIN;
+const clientId = AUTH0_CLIENT_ID;
 
 ReactDOM.render(
   <Auth0Provider
