82b4f0ffaf6c5b01d4563ac749adf935ab35b76e
Clint <maxeva02@gmail.com>
2021-05-26 15:43:52 -0700

    last

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,12 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
-var connection;
-//make connection
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-  connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "p298p57zjhjzof5d"
-  })
-}
-connection.connect();
-// Export connection for our ORM to use.
-module.exports = connection;
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
