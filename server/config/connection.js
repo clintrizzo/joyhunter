c79f1f2042b4659f3dfe1bf43fcd699f94d3b7be
Clint <maxeva02@gmail.com>
2021-05-26 15:55:27 -0700

    last

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,18 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
-var connection;
-//make connection
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-  connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "p298p57zjhjzof5d"
-  })
+const Sequelize = require('sequelize');
+require('dotenv').config();
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
 }
-connection.connect();
-// Export connection for our ORM to use.
-module.exports = connection;
-
+module.exports = sequelize;
